5491e21b15017d3ee13a7c6a89e250312c79bb9b
Farhan Baig <97299505+Farhancoader@users.noreply.github.com>
2026-06-29 12:07:07 +0530

    Time: 0 ms (100%), Space: 19.3 MB (58.44%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.py
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.py
@@ -1,25 +1,9 @@
-class node:
-    def __init__(self):
-        self.children = {}
-        self.ids = []
 class Solution:
     def numOfStrings(self, patterns: List[str], word: str) -> int:
-        root = node()
-        for idx,pattern in enumerate(patterns):
-            curr = root
-            for ch in pattern:
-                if ch not in curr.children:
-                    curr.children[ch]=node()
-                curr = curr.children[ch]
-            curr.ids.append(idx)
-        found = [False]*len(patterns)
-        for i in range(len(word)):
-            curr,j = root,i
-            while j<len(word) and word[j] in curr.children:
-                curr=curr.children[word[j]]
-                for idx in curr.ids:
-                    found[idx]=True
-                j+=1
-        return sum(found)
+        ans = 0
+        for pattern in patterns:
+            if pattern in word:
+                ans+=1
+        return ans
 
         
