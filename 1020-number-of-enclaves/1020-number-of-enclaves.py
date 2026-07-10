9b5e52d72d1617dcb11c32fe4833946bdc609992
Farhan Baig <97299505+Farhancoader@users.noreply.github.com>
2026-07-10 12:29:00 +0530

    Time: 61 ms (76.44%), Space: 37.3 MB (31.93%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,29 +1,25 @@
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        ones = 0
-        rows,cols = len(grid),len(grid[0])
-        for row in range(rows):
-            for col in range(cols):
-                if grid[row][col]==1:
-                    ones+=1
-        visit = set()
+        rows, cols = len(grid), len(grid[0])
         dirs = [(0,1),(1,0),(-1,0),(0,-1)]
-        def dfs(r,c):
-            nonlocal ones
-            if r<0 or r>=rows or c<0 or c>=cols or grid[r][c]==0 or (r,c) in visit:
+
+        def dfs(r, c):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == 0:
                 return
-            visit.add((r,c))
-            ones-=1
-            for dr,dc in dirs:
-                nr,nc = r+dr,c+dc
-                if 0<=nr<rows and 0<=nc<cols and (nr,nc) not in visit and grid[nr][nc]==1:
-                    dfs(nr,nc)
+
+            grid[r][c] = 0
+
+            for dr, dc in dirs:
+                dfs(r + dr, c + dc)
+
         for r in range(rows):
-            dfs(r,0)
-            dfs(r,cols-1)
+            dfs(r, 0)
+            dfs(r, cols - 1)
+
         for c in range(cols):
-            dfs(0,c)
-            dfs(rows-1,c)
-        return ones 
+            dfs(0, c)
+            dfs(rows - 1, c)
+
+        return sum(sum(row) for row in grid)
               
         
