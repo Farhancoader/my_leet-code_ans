6d61cfecab92b5ee73a73e3b98704408d3286b6f
Farhan Baig <97299505+Farhancoader@users.noreply.github.com>
2026-08-10 09:38:46 +0530

    Time: 1117 ms (5.01%), Space: 8.8 MB (90.65%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
-    bool winnerSquareGame(int n) {
-        vector<bool> dp(n+1,false);
-        for(int i=1;i<n+1;i++){
-            long long r = sqrt(i);
-            if(r*r==i){
-                dp[i]=true;
-                continue;
-            }
-            for(int j=1;j<=r;j++){
-                if(!dp[i-j*j]){
-                    dp[i]=true;
-                    break;
-                    }
-            }
+    int n = 1e5;
+    vector<bool> dp;
+    Solution(){
+        dp.resize(n+1);
+    for(int i=1;i<n+1;i++){
+        long long r = sqrt(i);
+        if(r*r==i){
+            dp[i]=true;
+            continue;
         }
-        return dp[n];
+        for(int j=1;j<=r;j++){
+            if(!dp[i-j*j]){
+                dp[i]=true;
+                break;
+                }
+        }
+    }
+    }
+    bool winnerSquareGame(int N) {
+        return dp[N];
     }
 };
