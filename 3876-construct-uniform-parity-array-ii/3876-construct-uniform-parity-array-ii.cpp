b89f2544be022262b9b2e03b495629ab53641702
Farhan Baig <97299505+Farhancoader@users.noreply.github.com>
2026-09-03 21:29:14 +0530

    Time: 0 ms (100%), Space: 165.7 MB (87.43%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        sort(nums1.begin(),nums1.end());
-        int first_odd=INT_MAX,first_even=INT_MAX;
-
-        for(int i=0;i<nums1.size();i++){
-            if(first_odd!=INT_MAX && first_even!=INT_MAX)break;
-            if(nums1[i]&1 && first_odd==INT_MAX)first_odd=i;
-            if(!(nums1[i]&1) && first_even==INT_MAX)first_even=i;
+        bool has_odd=false;
+        int mina = INT_MAX;
+        for(int num:nums1){
+            if(num&1)has_odd=true;
+            mina = min(mina,num);
         }
-        if(first_odd==INT_MAX || first_even==INT_MAX)return true;
-        if(first_odd<first_even)return true;
-        return false;
+        if(!has_odd)return true;
+        return mina&1;
     }
 };
